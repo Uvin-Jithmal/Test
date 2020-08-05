281ab8c7c32ab2a5d9f5aa9b6d8bfa29e8ba45d8
kalindu-laksahan <kalilaksahan@gmail.com>
2020-08-05 20:37:59 +0530

    intial commit

---
+++ app/src/main/java/com/example/usingstrings/ActivityOne.java
@@ -0,0 +1,67 @@
+package com.example.usingstrings;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+public class ActivityOne extends AppCompatActivity {
+
+
+    private static final String TAG = "ActivityOne";
+
+    TextView txtview2;
+
+   
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate: ");
+        
+        txtview2 = findViewById(R.id.textView2);
+       txtview2.setText(R.string.Msg2);
+
+
+
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        Log.i(TAG, "onStart: ");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop: ");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy: ");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume: ");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: ");
+    }
+
+}
